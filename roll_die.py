2d0d4b44c6fe30858dc35734435c666343c9b74d
Naincy Ghale <naincy.2n2k@gmail.com>
2026-01-01 23:06:53 +0545

    count add

--- roll_die.py
+++ roll_die.py
@@ -1,12 +1,16 @@
 import random
 
-x = "y"
+x = "yes"
+
+count1 = 0
+count2 = 0
+count3 = 0
+count4 = 0
+count5 = 0
+count6 = 0
  
-while x == "y":
+while x == "yes":
     
-    # Generates a random number
-    # between 1 and 6 (including
-    # both 1 and 6)
     no = random.randint(1,6)
     
     if no == 1:
@@ -16,36 +20,42 @@ while x == "y":
         print("|    0    |")
         print("|         |")
         print("|_________|")
+        count1 += 1
     if no == 2:
         print("[---------]")
         print("[  0      ]")
         print("[         ]")
         print("[      0  ]")
         print("[---------]")
+        count2 += 1
     if no == 3:
         print("[---------]")
         print("[0        ]")
         print("[    0    ]")
         print("[        0]")
         print("[---------]")
+        count3 += 1
     if no == 4:
         print("[---------]")
         print("[ 0     0 ]")
         print("[         ]")
         print("[ 0     0 ]")
         print("[---------]")
+        count4 += 1
     if no == 5:
         print("[---------]")
         print("[ 0     0 ]")
         print("[    0    ]")
         print("[ 0     0 ]")
         print("[---------]")
+        count5 += 1
     if no == 6:
         print("[---------]")
         print("[  0   0  ]")
         print("[  0   0  ]")
         print("[  0   0  ]")
         print("[---------]")
+        count6 += 1
         
-    x=input("press y to roll again and n to exit:")
+    x=input("Do you want to roll again? : ")
     print("\n")
