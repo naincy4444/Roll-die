616c7ca2de6c62cb00660e3409553cb9c027e613
Naincy Ghale <naincy.2n2k@gmail.com>
2026-01-02 22:31:05 +0545

    panda try

--- roll_die.py
+++ roll_die.py
@@ -3,33 +3,17 @@
 # Save the result in the database and show statistics.
 
 import random
-import sqlite3
-
-conn = sqlite3.connect("dice.db")
-cursor = conn.cursor()
-
-
-
-cursor.execute("""CREATE table die_roll(
-	total_rolls int primary key,
-	one int not null,
-    two int not null,
-    three int not null,
-    four int not null,
-    five int not null,
-    six int not null
-);
-""")
-
-x = "yes"
+import pandas as pd
 
 count1 = count2 = count3 = count4 = count5 = count6 = 0
-count_roll = 0
+total_roll = 0
+
+x = "yes"
 
 while x == "yes":
     
     no = random.randint(1,6)
-    count_roll += 1
+    total_roll += 1
     if no == 1:
         print( "_________") 
         print("|         |")
@@ -74,22 +58,21 @@ while x == "yes":
         print("[---------]")
         count6 += 1
 
-    cursor.execute("""
-    INSERT INTO die_roll (total_rolls, one, two, three, four, five, six)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, (count_roll, count1, count2, count3, count4, count5, count6))
-
-    conn.commit()
-
     x=input("Do you want to roll again? : ")
     print("\n")
 
+data = {
+    'Total roll': [total_roll],
+    '1': [count1],
+    '2': [count2],
+    '3': [count3],
+    '4': [count4],
+    '5': [count5],
+    '6': [count6]
+}
+
+df = pd.DataFrame(data)
 
 y = input("Do you want to know your statistics?")
 if y.lower() == "yes":
-    cursor.execute("SELECT * FROM die_roll")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-conn.close()
+    print(df)
